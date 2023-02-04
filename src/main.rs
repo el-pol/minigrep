8a46b06a23c60d16dbf380f3d86dac2708edc2cc
Pol Milian <pol.milian.dev@gmail.com>
2023-02-04 20:28:55 +0800

    using struct for clarity

--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,25 @@ use std::fs;
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let filename = &args[1];
-    let query = &args[2];
+    let config = Config::new(&args);
 
-    println!("arxiu: {}", filename);
-    println!("buscar: {}", query);
+    println!("arxiu: {}", config.filename);
+    println!("buscar: {}", config.query);
 
-    let content = fs::read_to_string(filename).expect("The file could not be read");
+    let content = fs::read_to_string(config.filename).expect("The file could not be read");
     println!("{}", &content[..30]);
+}
+
+struct Config {
+    filename: String,
+    query: String,
+}
+
+impl Config {
+    fn new(args: &[String]) -> Config {
+        let filename = args[1].clone();
+        let query = args[2].clone();
+
+        Config { filename, query }
+    }
 }
