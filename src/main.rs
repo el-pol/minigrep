2e6b68720288ab92103b41001f5cd4989789cc13
Pol Milian <pol.milian.dev@gmail.com>
2023-02-04 20:22:19 +0800

    read args, check file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use std::env;
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    let filename = &args[1];
+    let query = &args[2];
+
+    println!("arxiu: {}", filename);
+    println!("buscar: {}", query);
+
+    let content = fs::read_to_string(filename).expect("The file could not be read");
+    println!("{}", &content[..30]);
 }
